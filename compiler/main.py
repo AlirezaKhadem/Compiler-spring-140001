f719a54d2be1fbdccb19b9b572c259738972c5df
Matin <matinamini1991@yahoo.com>
2022-05-16 13:09:18 +0430

    Something.

--- compiler/main.py
+++ compiler/main.py
@@ -4,7 +4,6 @@ from compiler.parser.parser import Parser
 
 def run(input_file_address: str) -> str:
     parser = Parser(grammar=grammar, start=start, parser="lalr")
-
     result = parser.parse_file(input_file_address)
     if result == "OK":
         return True
--- compiler/scanner/patterns.py
+++ compiler/scanner/patterns.py
@@ -24,6 +24,7 @@ REVERSED_REGEX = {
     "Print",
     "private",
     "public",
+    "protected",
     "ReadInteger",
     "ReadLine",
     "return",
